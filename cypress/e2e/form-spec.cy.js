6fecd3cb91f45c761311405deeab514fbe5f5a0c
Jason Alberto <alberto.jason@gmail.com>
2023-07-29 17:30:02 -0400

    Add sad tests to the form tests

--- cypress/e2e/form-spec.cy.js
+++ cypress/e2e/form-spec.cy.js
@@ -18,4 +18,20 @@ describe('have a form that submits new ideas', () => {
         .get('.card').last().contains('p', 'My cool description goes here')
     })
   })
+
+  it('Should not display ideas if there is a 500 level network error', () => {
+    cy.intercept('GET', 'http://localhost:3001/ideas', {
+      statusCode: 500
+    })
+    cy.visit('http://localhost:3000/')
+     .get('.idea-container').find('.card').should('have.length', 0)
+  })
+
+  it('Should not display ideas if there is a 404 level network error', () => {
+    cy.intercept('GET', 'http://localhost:3001/ideas', {
+      statusCode: 404
+    })
+    cy.visit('http://localhost:3000/')
+     .get('.idea-container').find('.card').should('have.length', 0)
+  })
 })
