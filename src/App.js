ec252a3c279342507aede306cd664a89d52b2cd8
Jason Alberto <alberto.jason@gmail.com>
2023-07-29 17:11:02 -0400

    Form submit is displayed on dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { getItems } from './apiCalls';
 import { useState, useEffect } from 'react';
 import IdeaList from './components/IdeaList/IdeaList';
+import Form from './components/Form/Form';
 
 function App() {
   const [items, setItems] = useState([])
@@ -19,13 +20,22 @@ function App() {
     })()    
   }, [])
 
+  const addItem = (newItem) => {
+    setItems(prevItems => [... prevItems, newItem])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>IDEA-BOX</h1>
       </header>
       <main>
-        <IdeaList items={items} />
+        <div className='form-container'>
+          <Form addItem={addItem} />
+        </div>
+        <div className='idea-container'>
+          <IdeaList items={items} />
+        </div>
       </main>
     </div>
   );
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,18 +1,28 @@
 import { useState } from "react"
 
-const Form = () => {
+const Form = ({addItem}) => {
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
 
+    const newItem = {
+      id: Date.now(),
+      title,
+      description
+    }
+    setDescription('')
+    setTitle('')
+
+    addItem(newItem)
   }
 
   return (
     <form name="item" >
       <input name="title" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Enter Title" />
       <input name="description" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Enter Description" />
-      <button onClick={(e) => handleClick(e)} />
+      <button onClick={(e) => handleClick(e)}>SUBMIT!</button>
     </form>
   )
 }
