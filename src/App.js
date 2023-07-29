ebd20b9ab9e039d6884aa64cf397f7229106b66f
Jason Alberto <alberto.jason@gmail.com>
2023-07-29 16:41:22 -0400

    Get data and save to state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import { useState, useEffect } from 'react';
 function App() {
   const [items, setItems] = useState([])
 
+  useEffect(() => {
+    (async() => {
+      try {
+        const data = await getItems()
+        console.log(data)
+        setItems(data)
+      } catch (error) {
+        console.log(error.message)
+      }
+    })()    
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
